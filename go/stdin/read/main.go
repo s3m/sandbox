fbac2bcbbbdeaf2469f93a4700abc5d8ee535797
nbari <nbari@tequila.io>
2019-04-07 10:07:28 +0200

    sync Sun Apr  7 10:07:28 CEST 2019

--- go/stdin/read/main.go
+++ go/stdin/read/main.go
@@ -1,34 +1,34 @@
 package main
 
 import (
-    "bufio"
-    "io"
-    "log"
-    "os"
+	"bufio"
+	"io"
+	"log"
+	"os"
 )
 
 func main() {
-    nBytes, nChunks := int64(0), int64(0)
-    r := bufio.NewReader(os.Stdin)
-    buf := make([]byte, 0, 4*1024)
-    for {
-        n, err := r.Read(buf[:cap(buf)])
-        buf = buf[:n]
-        if n == 0 {
-            if err == nil {
-                continue
-            }
-            if err == io.EOF {
-                break
-            }
-            log.Fatal(err)
-        }
-        nChunks++
-        nBytes += int64(len(buf))
-        // process buf
-        if err != nil && err != io.EOF {
-            log.Fatal(err)
-        }
-    }
-    log.Println("Bytes:", nBytes, "Chunks:", nChunks)
+	nBytes, nChunks := int64(0), int64(0)
+	r := bufio.NewReader(os.Stdin)
+	buf := make([]byte, 0, 1024)
+	for {
+		n, err := r.Read(buf[:cap(buf)])
+		buf = buf[:n]
+		if n == 0 {
+			if err == nil {
+				continue
+			}
+			if err == io.EOF {
+				break
+			}
+			log.Fatal(err)
+		}
+		nChunks++
+		nBytes += int64(len(buf))
+		// process buf
+		if err != nil && err != io.EOF {
+			log.Fatal(err)
+		}
+	}
+	log.Println("Bytes:", nBytes, "Chunks:", nChunks)
 }
