513ab1ee4c96d1066c391f362a8e3a1129d8717a
nbari <nbari@tequila.io>
2019-04-07 14:01:42 +0200

    sync Sun Apr  7 14:01:42 CEST 2019

--- go/stdin/scanner/main.go
+++ go/stdin/scanner/main.go
@@ -5,29 +5,34 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"time"
 )
 
-func main() {
-	stat, _ := os.Stdin.Stat()
-	if (stat.Mode() & os.ModeCharDevice) == 0 {
-		scanner := bufio.NewScanner(os.Stdin)
-		scanner.Split(bufio.ScanBytes)
-		chunk := make([]byte, 0, 1024)
-		for scanner.Scan() {
-			chunk = append(chunk, scanner.Bytes()...)
-			if len(chunk) == 1024 {
-				fmt.Printf("chunk: %s\n", chunk)
-				chunk = nil
-				time.Sleep(time.Second)
-			}
-		}
-		if err := scanner.Err(); err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("last chunk: %s len: %d\n", chunk, len(chunk))
-	} else {
-		fmt.Fprintln(os.Stderr, "nothing in stdin, pipe somethinig")
-		os.Exit(1)
+func ScanBytes(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	if atEOF && len(data) == 0 {
+		return 0, nil, nil
 	}
+	if len(data) >= 4 {
+		return 4, data[0:4], nil
+	}
+	return 0, data[0:], nil
+}
+
+func main() {
+	nBytes, nChunks := int64(0), int64(0)
+	scanner := bufio.NewScanner(os.Stdin)
+	//buf := make([]byte, 0, 1024)
+	//scanner.Buffer(buf, 10<<20)
+	//	chunk := make([]byte, 0, 1024)
+	scanner.Split(ScanBytes)
+	for scanner.Scan() {
+		fmt.Printf("scanner.Bytes() = %s\n", scanner.Bytes())
+		//buf = append(buf, scanner.Bytes()...)
+		nChunks++
+		//		nBytes += int64(len(buf))
+		//buf = nil
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Bytes:", nBytes, "Chunks:", nChunks)
 }
